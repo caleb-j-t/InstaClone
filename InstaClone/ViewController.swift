ed1a695cee7b9996e684d2f6aa92e3bd6357b11c
Caleb Talbot <mailcaleb@gmail.com>
2016-06-17 17:25:01 -0500

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Caleb Talbot on 6/17/16.
+//  Copyright © 2016 Caleb Talbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
