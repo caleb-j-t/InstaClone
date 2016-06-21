b5c29bf4fb050c7666fbd6fe9da077ddfcd32ee9
Sean1736 <milkwood72@gmail.com>
2016-06-21 13:37:24 -0500

    User and Pass working, facebook not.

--- InstaClone/LoginViewController.swift
+++ InstaClone/LoginViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 import FBSDKLoginKit
 // User FirebaseAuth to allow firebase to see users logging in.
+import Firebase
 import FirebaseAuth
 
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    
+    
+    @IBOutlet weak var userNameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+
     
     
     
@@ -24,15 +32,30 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
         self.loginButton.hidden = true
         
         
+        
+        
+        // Not working
+//        func regularLoginField() {
+//            if userNameField.text == "" && passwordField.text == ""{
+//                self.view.backgroundColor = UIColor.blackColor()
+//            } else {
+//                self.view.backgroundColor = UIColor.redColor()
+//            }
+//            
+//        }
+//        
+        
         // This code will check to see if the user is signed in or not. Located in firebase manage users: Step 1.
         FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
             if let user = user {
                 // If the user is signed in, show the home page.
                 
-                let mainStoryBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                let homeViewController: UIViewController = mainStoryBoard.instantiateViewControllerWithIdentifier("HomeView")
+                let loginStoryBoard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+                // Uncomment this when we get feed done and add HomeView as the storyboard id.
                 
-                self.presentViewController(homeViewController, animated: true, completion: nil)
+                let profileViewController: UIViewController = loginStoryBoard.instantiateViewControllerWithIdentifier("ProfileView")
+                
+                self.presentViewController(profileViewController, animated: true, completion: nil)
                 
             } else {
                 // If user is signed out, show the login button.
@@ -44,6 +67,9 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
                 self.view!.addSubview(self.loginButton)
                 
                 self.loginButton.hidden = false
+                
+                
+                
             }
             
         }
@@ -51,12 +77,13 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        print("user logged in")
+        
         
         self.loginButton.hidden = true
-        
-        
-        
+//        if self.loginButton.hidden == true {
+//            } else {
+//            print("Facebook Not Logged In")
+//        }
         
         // Code to deal with users who hit cancel on the facebook login access.
         if (error != nil)
@@ -69,7 +96,8 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
             self.loginButton.hidden = false
             
         } else {  // User hits OK to grant rights to use Facebook Login.
-            
+            print("user logged in")
+
             // Since Firebase cannot see users logged in even with facebook set up correctly, we have to add this code. Located in guides, auth, facebook, step 4.
             
             // This is getting an access token for the signed-in user and exchanging it for a Firebase credential:
@@ -79,6 +107,10 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
             // Step 5 authenticate with Firebase using the Firebase credential
             FIRAuth.auth()?.signInWithCredential(credential) { (user, error) in
                 print("user logged into firebase")
+                self.performSegueWithIdentifier("toProfile", sender: self)
+
+                
+                
                 
             }
             
@@ -90,5 +122,30 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     }
     
     
+   
+    
+    @IBAction func loginButtonTapped(sender: AnyObject) {
+  
+            FIRAuth.auth()?.signInWithEmail(userNameField.text!, password: passwordField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("User Logged In")
+                    
+//                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//                    let vc = storyboard.instantiateViewControllerWithIdentifier("ProfileView")
+//                    self.presentViewController(vc, animated: true, completion: nil)
+                       self.performSegueWithIdentifier("toProfile", sender: self)
+                           
+                }
+                else {
+                    print("Invalid Login")
+                    
+                }
+                
+            }
+        }
+
+    @IBAction func unwindToMenu(segue: UIStoryboardSegue) {}
+
 }
 
---
+++ InstaClone/PhotoDisplayViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoDisplayViewController.swift
+//  InstaClone
+//
+//  Created by Cindy Barnsdale on 6/20/16.
+//  Copyright © 2016 Caleb Talbot. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDisplayViewController: UITableViewController {
+
+  //  @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // We set the table view header.
+//        let cellTableViewHeader = tableView.dequeueReusableCellWithIdentifier(TableViewController.tableViewHeaderCustomCellIdentifier) as! UITableViewCell
+//        cellTableViewHeader.frame = CGRectMake(0, 0, self.tableView.bounds.width, self.heightCache[TableViewController.tableViewHeaderCustomCellIdentifier]!)
+//        self.tableView.tableHeaderView = cellTableViewHeader
+//        
+//        // We set the table view footer, just know that it will also remove extra cells from tableview.
+//        let cellTableViewFooter = tableView.dequeueReusableCellWithIdentifier(TableViewController.tableViewFooterCustomCellIdentifier) as! UITableViewCell
+//        cellTableViewFooter.frame = CGRectMake(0, 0, self.tableView.bounds.width, self.heightCache[TableViewController.tableViewFooterCustomCellIdentifier]!)
+//        self.tableView.tableFooterView = cellTableViewFooter
+//    
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
