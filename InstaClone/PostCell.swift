6d3f7db59ee9bfd01cb56d6a9a8afcc550367681
Sean1736 <milkwood72@gmail.com>
2016-06-23 20:17:03 -0500

    Constraints fixed with Caleb changes

--- InstaClone/PostCell.swift
+++ InstaClone/PostCell.swift
@@ -6,30 +6,30 @@
 //  Copyright © 2016 Caleb Talbot. All rights reserved.
 //
 
-import UIKit
-
-class PostCell: UITableViewCell {
-
-    @IBOutlet weak var userImage: UIImageView!
-    @IBOutlet weak var usernameButton: UIButton!
-    @IBOutlet weak var postPic: UIImageView!
-    @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var totalLikes: UILabel!
-    @IBOutlet weak var totalComments: UILabel!
-    @IBOutlet weak var commentPreviews: UITextView!
-    var comment = ""
-    
-    override func awakeFromNib() {
-        
-        
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+//import UIKit
+//
+//class PostCell: UITableViewCell {
+//
+//    @IBOutlet weak var userImage: UIImageView!
+//    @IBOutlet weak var usernameButton: UIButton!
+//    @IBOutlet weak var postPic: UIImageView!
+//    @IBOutlet weak var likeButton: UIButton!
+//    @IBOutlet weak var totalLikes: UILabel!
+//    @IBOutlet weak var totalComments: UILabel!
+//    @IBOutlet weak var commentPreviews: UITextView!
+//    var comment = ""
+//    
+//    override func awakeFromNib() {
+//        
+//        
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
